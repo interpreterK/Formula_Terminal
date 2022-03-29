5afad5372ca5eaff1571507af2aed91ad61b54d8
interpreterK <gamingdudeaz@gmail.com>
2022-03-29 11:46:05 -0400

    Attempt Changes

--- script.cpp
+++ script.cpp
@@ -6,9 +6,18 @@ using namespace std;
 
 int points = 0;
 
+//cd "/Users/brusso/Desktop/C++:C/" && g++ script.cpp -o script && "/Users/brusso/Desktop/C++:C/"script
+
 int randn(int maxn) 
 { 
   return rand() % maxn + 1;
+}
+void level(string levelName, int maxn, int uGuess, int uNum)
+{
+  printf("[Level %s]: Guess the number (1 to %s): ", &levelName, to_string(maxn));
+  scanf("%d", &uGuess);
+  if (uNum < uGuess) puts("The secret number is lower");
+  else if (uNum > uGuess) puts("The secret number is higher");
 }
 int level2()
 {
@@ -16,7 +25,7 @@ int level2()
   srand(time(NULL));
   uNum = randn(100);
 
-  do { 
+  do {
 
   } while (uNum != uGuess);
   return 0;
@@ -28,10 +37,7 @@ int main() // Level 1
   uNum = randn(10);
 
   do {
-    printf("[Level 1]: Guess the number (1 to 10): ");
-    scanf("%d", &uGuess);
-    if (uNum < uGuess) puts("The secret number is lower");
-    else if (uNum > uGuess) puts("The secret number is higher");
+    level("1", 10, uGuess, uNum);
   } while (uNum != uGuess);
 
   puts("Congratulations!");
