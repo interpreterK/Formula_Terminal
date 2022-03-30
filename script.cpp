d8a4f0889a59f986c6c65daa47f879d6c7979ae4
interpreterK <gamingdudeaz@gmail.com>
2022-03-30 12:45:58 -0400

    Formula Terminal Creation start

--- script.cpp
+++ script.cpp
@@ -1,47 +1,53 @@
+#include <iostream>
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <string>
+#include <ctype.h>
 using namespace std;
 
-int points = 0;
+bool Closing = false;
 
-//cd "/Users/brusso/Desktop/C++:C/" && g++ script.cpp -o script && "/Users/brusso/Desktop/C++:C/"script
+class Commands {
+  public:
+   void clear() {
+     cout << "\x1B[2J\x1B[H";
+   }
+   void help() {
+     cout << "--------" << endl;
+     cout << "clear - Clears this prompt" << endl << "help - List of all commands" << endl << "exit - Exits this prompt" << endl;
+     cout << "--------" << endl;
+   }
+   void exit() {
+     Closing = true;
+   }
+   double between(double x, double y) {
+     return (x + y) / 2;
+   }
+};
 
-int randn(int maxn) 
-{ 
-  return rand() % maxn + 1;
-}
-void level(string levelName, int maxn, int uGuess, int uNum)
-{
-  printf("[Level %s]: Guess the number (1 to %s): ", &levelName, to_string(maxn));
-  scanf("%d", &uGuess);
-  if (uNum < uGuess) puts("The secret number is lower");
-  else if (uNum > uGuess) puts("The secret number is higher");
-}
-int level2()
-{
-  int uNum, uGuess;
-  srand(time(NULL));
-  uNum = randn(100);
+int main(int argc, char** argv[]) {
+  Commands Actions;
+  string inp;
 
-  do {
-
-  } while (uNum != uGuess);
-  return 0;
-}
-int main() // Level 1
-{
-  int uNum, uGuess;
-  srand(time(NULL));
-  uNum = randn(10);
+  Actions.clear(); //Clear once the console is launched.
+  cout << "--------" << endl << "-+*/= Formula Terminal =/*+-" << endl;
+  Actions.help(); //List the commands once the console is opened.
 
   do {
-    level("1", 10, uGuess, uNum);
-  } while (uNum != uGuess);
+    cin >> inp;
 
-  puts("Congratulations!");
-  points += 5;
-  level2();
+    if (inp == "help") {
+      Actions.help();
+    } else if (inp == "clear") {
+      Actions.clear();
+    } else if (inp == "exit") {
+      Actions.exit();
+    } else if (inp == "between") {
+      
+    } else {
+      cout << "Unknown Command: " + inp << endl;
+    }
+  } while (!Closing);
+
+  cout << "Exiting..." << endl;
+
   return 0;
 }
